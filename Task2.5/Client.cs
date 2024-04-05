e67cb23f1bf0023557f805372f2c578818edd509
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2024-04-05 11:39:17 +0700

    jhdfhgfhjfh

---
+++ Task2.5/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApplication8
+{
+    public class Client
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }   
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }                
+            set { age = value; }
+        }
+
+        public Client() : this("Не указанно")
+        {
+        
+        }
+
+        public Client(string name) : this(name, 18)
+        {
+        
+        }
+
+        public Client(string name, int age)
+        {
+            this.name = name;     
+            this.age = age;
+        }
+
+        
+
+        ~Client()
+        {
+            Console.WriteLine($"{name} has deleted");
+        }
+    }
+}
---
+++ Task2.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConsoleApplication8;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Client hinata = new Client();
+        Console.WriteLine($"Имя: {hinata.Name}, возраст: {hinata.Age}");
+        Client sakura= new Client("Sakura");
+        Console.WriteLine($"Имя: {sakura.Name}, возраст: {sakura.Age}");
+        Client itaci = new Client("Itaci", 10000);
+        Console.WriteLine($"Имя: {itaci.Name}, возраст: {itaci.Age}");
+       
+       
+    }
+}
+
